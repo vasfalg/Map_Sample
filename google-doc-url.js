4971f65fb20f6c52132c128d994245a6b65027a7
vasfalg <92820437+vasfalg@users.noreply.github.com>
2023-12-12 14:08:14 -0500

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1SdXUL93t64hvsVBbn8ui-2stBFkS1IKSZWR20dCU3OQ/edit?usp=sharing';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyCT9ge3U_GCLv65E-_rz6Iqrl6EJ61JaVk';
